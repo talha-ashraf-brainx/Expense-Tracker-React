31ef62655e0c09d842b65ef20cf8df649ba7d6d5
Talha Ashraf <talha.ashraf@brainxtech.com>
2026-04-09 14:35:13 +0500

    available balance row

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import AppBar from './components/AppBar'
+import AvailableBalance from './components/AvailableBalance'
 
 function App() {
 
   return (
     <>
-  <AppBar />
+      <AppBar />
+      <AvailableBalance />
     </>
   )
 }
---
+++ src/components/AvailableBalance.tsx
@@ -0,0 +1,16 @@
+import './AvailableBalance.css'
+
+function AvailableBalance() {
+    return (
+        <div className="available-balance">
+            <h4>
+                TOTAL AVAILABLE BALANCE
+            </h4>
+            <h1>
+                $10,000.00
+            </h1>
+        </div>
+    )
+}
+
+export default AvailableBalance
